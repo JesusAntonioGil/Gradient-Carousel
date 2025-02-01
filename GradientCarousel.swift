d64c1b9f0aec1966fcf8280a94f4504bc487292f
Jesús Antonio Gil <jesus.a22@hotmail.com>
2025-02-01 11:48:23 +0100

    Image model file

--- GradientCarousel.swift
+++ GradientCarousel.swift
@@ -153,18 +153,3 @@ struct GradientCarousel: View {
 }
 
 
-
-
-struct ImageModel: Identifiable {
-    let id: String = UUID().uuidString
-    let altText: String
-    let image: String
-}
-
-
-let images: [ImageModel] = [
-    .init(altText: "Mo Eid", image: "Pic 1"),
-    .init(altText: "Codioful", image: "Pic 2"),
-    .init(altText: "Cottonbro", image: "Pic 3"),
-    .init(altText: "Anni", image: "Pic 4")
-]
